dfdfd6289cae84190deb250a35d0a55a53657154
Oleg Marchik <oleg.marchik@almus.app>
2024-01-15 15:41:19 +0200

    remove unused code base

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -9,68 +9,8 @@ import SwiftUI
 import OpenAIStreamingCompletions
 
 struct ContentView: View {
-    @State private var prompt = "what is internet explorer"
-    @State private var completion: StreamingCompletion?
-    @State private var completedText: String = ""
-    @AppStorage("key") private var key = ""
-
     var body: some View {
-        Form {
-            Section {
-                TextField("API key", text: $key)
-                TextField("Prompt to complete", text: $prompt, onCommit: complete)
-                Button("Complete Text", action: complete)
-                Button("Complete Chat", action: completeChat)
-            }
-            if let completion {
-                Section {
-                    CompletionView(completion: completion)
-                }
-            }
-            if completedText != "" {
-                Section {
-                    Text(completedText)
-                }
-            }
-        }
-    }
-
-    private func complete() {
-        if key == "" { return }
-        self.completion = try! OpenAIAPI(apiKey: key).completeStreaming(.init(prompt: prompt, max_tokens: 256))
-    }
-
-    private func completeChat() {
-        if key == "" { return }
-        let messages: [OpenAIAPI.Message] = [
-            .init(role: .system, content: "You are a helpful assistant. Answer in one sentence if possible."),
-            .init(role: .user, content: prompt)
-        ]
-//        Task {
-//            do {
-//                self.completedText = try await OpenAIAPI(apiKey: key).completeChat(.init(messages: messages))
-//            } catch {
-//                self.completedText = "Error: \(error)"
-//            }
-//        }
-        self.completion = try! OpenAIAPI(apiKey: key).completeChatStreamingWithObservableObject(.init(messages: messages))
-    }
-}
-
-private struct CompletionView: View {
-    @ObservedObject var completion: StreamingCompletion
-
-    var body: some View {
-        Group {
-            Text("\(completion.text)")
-                .multilineTextAlignment(.leading)
-                .lineLimit(nil)
-        }
-        switch completion.status {
-        case .error: Text("Errror")
-        case .complete: Text("Complete")
-        case .loading: Text("Loading")
-        }
+        EmptyView()
     }
 }
 
--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OpenAIStreamingCompletions",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/OpenAIStreamingCompletions/EventSource"),
         .testTarget(
             name: "OpenAIStreamingCompletionsTests",
             dependencies: ["OpenAIStreamingCompletions"]),
